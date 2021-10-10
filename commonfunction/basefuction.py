4dd6243372e8e5cb2cc3b3526bf2239681ed016c
darlychoi <81666953@qq.com>
2021-10-10 12:22:42 +0800

    darlychoi ask why delete content

--- commonfunction/basefuction.py
+++ commonfunction/basefuction.py
@@ -1,4 +1,4 @@
 modifaciton by choi darly
 7777
 delete somthing by choi darly
-The previous content by choi darly
+The previous content by choi darly   (darlychoi edit:why did you delete my content)
